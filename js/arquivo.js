dda623135af73149bcf00797a7c9caf21d26d8bd
Maria Clara <mariaclara2016085@gmail.com>
2026-06-19 14:04:59 -0300

    crud em arquivo

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,45 +1,27 @@
 class Arquivo {
+  constructor() {
+    // Representa o conteúdo do arquivo em bytes
+    this.bytes = [];
 
-    constructor() {
+    // Recupera os dados salvos anteriormente
+    this.carregar();
+  }
 
-        // Representa o conteúdo do arquivo em bytes
-        this.bytes = []
+  // Salva o vetor de bytes no LocalStorage
+  salvar() {
+    localStorage.setItem("arquivo", JSON.stringify(this.bytes));
+  }
 
-        // Recupera os dados salvos anteriormente
-        this.carregar()
+  // Carrega o vetor salvo
+  carregar() {
+    const dados = localStorage.getItem("arquivo");
+
+    if (dados) {
+      this.bytes = JSON.parse(dados);
     }
+  }
 
-
-    // Salva o vetor de bytes no LocalStorage
-    salvar() {
-
-        localStorage.setItem(
-            "arquivo",
-            JSON.stringify(this.bytes)
-        )
-
-    }
-
-
-    // Carrega o vetor salvo
-    carregar() {
-
-        const dados =
-            localStorage.getItem(
-                "arquivo"
-            )
-
-        if (dados) {
-
-            this.bytes =
-                JSON.parse(dados)
-
-        }
-
-    }
-
-
-    /*
+  /*
         Estrutura do registro:
 
         [LÁPIDE]
@@ -47,23 +29,16 @@ class Arquivo {
         [DADOS]
     */
 
-    adicionarRegistro(dados) {
+  adicionarRegistro(dados) {
+    const lapide = 0;
+    const tamanho = dados.length;
 
-        const lapide = 0
-        const tamanho = dados.length
+    this.bytes.push(lapide, tamanho, ...dados);
 
-        this.bytes.push(
-            lapide,
-            tamanho,
-            ...dados
-        )
+    this.salvar();
+  }
 
-        this.salvar()
-
-    }
-
-
-    /*
+  /*
         Percorre o arquivo:
 
         lê cabeçalho
@@ -71,60 +46,106 @@ class Arquivo {
         reconstrói Produto
     */
 
-    listar() {
+  listar() {
+    const produtos = [];
 
-        const produtos = []
+    let posicao = 0;
 
-        let posicao = 0
+    while (posicao < this.bytes.length) {
+      // Cabeçalho
+      const lapide = this.bytes[posicao];
 
-        while (
-            posicao <
-            this.bytes.length
-        ) {
+      const tamanho = this.bytes[posicao + 1];
 
-            // Cabeçalho
-            const lapide =
-                this.bytes[posicao]
+      // Conteúdo do registro
+      const dados = this.bytes.slice(posicao + 2, posicao + 2 + tamanho);
 
-            const tamanho =
-                this.bytes[
-                    posicao + 1
-                ]
+      // Só mostra registros ativos
+      if (lapide === 0) {
+        produtos.push(Produto.desserializar(dados));
+      }
 
-
-            // Conteúdo do registro
-            const dados =
-                this.bytes.slice(
-                    posicao + 2,
-                    posicao + 2 + tamanho
-                )
-
-
-            // Só mostra registros ativos
-            if (
-                lapide === 0
-            ) {
-
-                produtos.push(
-
-                    Produto
-                        .desserializar(
-                            dados
-                        )
-
-                )
-
-            }
-
-
-            // Vai para próximo registro
-            posicao +=
-                tamanho + 2
-
-        }
-
-        return produtos
-
+      // Vai para próximo registro
+      posicao += tamanho + 2;
     }
 
+    return produtos;
+  }
+
+  /*
+    Procura um registro pelo ID no vetor de bytes
+    Retorna o objeto Produto se for encontrado e estiver ativo
+     */
+  buscar(id) {
+    let posicao = 0;
+
+    while (posicao < this.bytes.length) {
+      const lapide = this.bytes[posicao];
+      const tamanho = this.byte[posicao + 1];
+      //pega dados que comecam de posicao + 2  ate o fim
+      const dados = this.bytes.slice(posicao + 2, ...[posicao + 2 + tamanho]);
+
+      if (lapide == 0) {
+        const produto = Produto.desserializar(dados);
+        if (produto.id === id) {
+          return produto; //retorna se encontrado
+        }
+      }
+      posicao += tamanho + 2;
+    }
+    return null; //caso nao seja encontrado
+  }
+
+  /*
+    Exclusão lógica com lápide, procurando o registro
+    e alterando o byte lápide para 1
+     */
+  excluir(id) {
+    let posicao = 0;
+
+    while (posicao < this.bytes.length) {
+      const lapide = this.bytes[posicao];
+      const tamanho = this.bytes[posicao + 1];
+      const dados = this.bytes.slice(posicao + 2, ...[posicao + 2 + tamanho]);
+
+      if (lapide === 0) {
+        const produto = Produto.desserializar(dados);
+        if (produto.id === id) {
+          this.bytes[posicao] = 1; //altera a lapide para 1
+          this.salvar(); //salva alteracao
+          return true;
+        }
+      }
+      posicao += tamanho + 2;
+    }
+
+    return false; //se nao achar ou se já estiver excluido
+  }
+
+  /*
+  Alteração do registro
+  */
+  alterar(id, novoProduto) {
+    let posicao = 0;
+
+    while (posicao < this.bytes.length) {
+      const lapide = this.bytes[posicao];
+      const tamanho = this.bytes[posicao + 1];
+      const dados = this.bytes.slice(posicao + 2, ...[posicao + 2 + tamanho]);
+
+      if (lapide === 0) {
+        const produto = Produto.desserializar(dados);
+        if (produto.id === id) {
+          this.bytes[posicao] = 1; //marca como excluido o registor antigo
+
+          const novosBytes = novoProduto.serializar();
+          this.adicionarRegistro(novosBytes); //salva
+          return true;
+        }
+      }
+      posicao += tamanho + 2;
+    }
+
+    return false; //se nao acha produto para alterar
+  }
 }
